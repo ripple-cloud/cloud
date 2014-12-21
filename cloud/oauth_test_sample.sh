f8349db49ce3421b17e07951da08aac5fa0eb4b7
Audrey <audreylh@gmail.com>
2014-12-21 22:24:43 +0800

    cover conditional for unknown errors, eg. server not running

--- cloud/oauth_test_sample.sh
+++ cloud/oauth_test_sample.sh
@@ -5,13 +5,16 @@
 
 function test {
     res=`curl -s -I -X POST $1 | grep HTTP/1.1 | awk {'print $2'}`
-    if [ $res -ne 200 ]
+    if [ $res -eq 200 ]
+    then
+      echo "PASS: $res"
+    elif [ $res -ne 200 ]
     then
         echo "FAIL!"
         echo "Error $res on $1"
         curl -X POST $1 
     else
-        echo "PASS: $res"
+        echo "UNKNOWN ERROR"
     fi
 }  
 
