e32b3668b602c1ce247e96fa265ef869a2df9d02
Lakshan Perera <lakshan@laktek.com>
2015-01-11 06:41:44 +0800

    Combine handlers/common and handlers/errors into handlers/responders

--- handlers/responders.go
+++ handlers/responders.go
@@ -37,3 +37,15 @@ func serverError(w http.ResponseWriter, r *http.Request, err error) {
 	}
 	w.Wrtite(o)
 }
+
+func respondJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(code)
+
+	o, err := json.Marshal(payload)
+	if err != nil {
+		log.Infof("[error] respondJSON: %s", err)
+		return
+	}
+	w.Wrtite(o)
+}
