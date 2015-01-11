f92759541e1fdfc7477d78d92240fff3a421385d
Lakshan Perera <lakshan@laktek.com>
2015-01-12 07:22:01 +0800

    Added JSON annotations to User struct.

--- data/user.go
+++ data/user.go
@@ -9,12 +9,12 @@ import (
 )
 
 type User struct {
-	ID                int64      `db:"id"`
-	Username          string     `db:"username"`
-	Email             string     `db:"email"`
-	EncryptedPassword string     `db:"encrypted_password"`
-	CreatedAt         *time.Time `db:"created_at"`
-	UpdatedAt         *time.Time `db:"updated_at"`
+	ID                int64      `db:"id" json:"id"`
+	Username          string     `db:"username" json:"username"`
+	Email             string     `db:"email" json:"email"`
+	EncryptedPassword string     `db:"encrypted_password" json:"-"`
+	CreatedAt         *time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt         *time.Time `db:"updated_at" json:"updated_at"`
 }
 
 func (u *User) EncryptPassword(password string) error {
